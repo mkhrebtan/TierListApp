85c7c0b847c080a416640eefdc49b3881d615454
Maksym Khrebtan <m.khrebtan21@gmail.com>
2025-06-16 12:08:45 +0300

    Implement drag-and-drop functionality with data management updates

--- Client/js/drag.js
+++ Client/js/drag.js
@@ -6,7 +6,42 @@ function initializeDragAndDrop() {
             group: 'shared',
             animation: 300,
             ghostClass: 'sortable-ghost',
+            chosenClass: "sortable-chosen",
+            onEnd: function (evt) {
+                updateDataAfterDrop(evt);
+            }
         });
     });
+}
+
+function updateDataAfterDrop(evt) {
+    if (!evt.item) return;
+
+    const imageId = parseInt(evt.item.id.replace('img', ''));
+    const newIndex = evt.newIndex;
+    let fromBoxId = null;
+
+    if (evt.from.id === 'backup-drop-box') {
+        fromBoxId = 'backup-drop-box';
+    }
+    else {
+        fromBoxId = parseInt(evt.from.id.replace('drop-box-', ''));
+    }
+    
+    if (evt.from === evt.to) {
+        dataManager.updateImageOrder(fromBoxId, imageId, newIndex);
+    }
+    else {
+        let toBoxId = null;
+        if (evt.to.id === 'backup-drop-box') {
+            toBoxId = 'backup-drop-box';
+        }
+        else {
+            toBoxId = parseInt(evt.to.id.replace('drop-box-', ''));
+        }
+        dataManager.moveImage(imageId, fromBoxId, toBoxId, newIndex);
+    }
+    
+    console.log(dataManager.toAPIFormat());
 }
 
