789771933185460cb8ef9ed47925090a22774fd1
Maksym Khrebtan <m.khrebtan21@gmail.com>
2025-06-28 00:50:15 +0300

    Add Entity Framework Core compiled models configuration

--- API/TierList.Persistence.Postgres/TierListDbContext.cs
+++ API/TierList.Persistence.Postgres/TierListDbContext.cs
@@ -14,6 +14,16 @@ public class TierListDbContext : DbContext
 
     public TierListDbContext(DbContextOptions<TierListDbContext> options) : base(options) { }
 
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Database=TierList;Username=Dev;Password=Makc2006789333");
+        }
+
+        optionsBuilder.UseModel(CompiledModels.TierListDbContextModel.Instance);
+    }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfiguration(new TierListEntityConfiguration());
