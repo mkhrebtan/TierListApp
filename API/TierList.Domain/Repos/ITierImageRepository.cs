0bc023eb7ae8261a262ec3cfe4eb87b8b2f59c69
Maksym Khrebtan <m.khrebtan21@gmail.com>
2025-06-27 11:47:33 +0300

    Refactor: Remove tier image and tier image container repository interfaces and class

--- API/TierList.Domain/Repos/ITierImageRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using TierList.Domain.Abstraction;
-using TierList.Domain.Entities;
-
-namespace TierList.Domain.Repos;
-
-public interface ITierImageRepository : IRepository<TierImageEntity>
-{
-    IEnumerable<TierImageEntity> GetRowImages(int rowId);
-}
