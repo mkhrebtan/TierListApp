871baae32428996044babaf125e8a55bce7ca5b2
Maksym Khrebtan <m.khrebtan21@gmail.com>
2025-06-29 13:14:40 +0300

    Add ITierListDTO interface and update TierListResult
    Created the ITierListDTO interface with Id and Title properties.
    Updated TierListResult to use ITierListDTO for TierListData
    and modified the Success method accordingly.

---
+++ API/TierList.Application/Common/Interfaces/ITierListDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TierList.Application.Common.Interfaces;
+
+public interface ITierListDTO
+{
+    int Id { get; init; }
+
+    string Title { get; init; }
+}
--- API/TierList.Application/Common/Models/TierListResult.cs
+++ API/TierList.Application/Common/Models/TierListResult.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TierList.Application.Common.Enums;
+using TierList.Application.Common.Interfaces;
 using TierList.Application.Queries.DTOs;
 using TierList.Domain.Entities;
 
@@ -14,8 +15,8 @@ public record TierListResult
     public bool IsSuccess { get; init; }
     public string? ErrorMessage { get; init; }
     public ErrorType? ErrorType { get; init; }
-    public TierListBriefDTO? TierListData { get; init; }
+    public ITierListDTO? TierListData { get; init; }
 
-    public static TierListResult Success(TierListBriefDTO? data = null) => new () { IsSuccess = true, TierListData = data ?? null };
+    public static TierListResult Success(ITierListDTO? data = null) => new () { IsSuccess = true, TierListData = data ?? null };
     public static TierListResult Failure(string error, ErrorType errorType) => new () { IsSuccess = false, ErrorMessage = error, ErrorType = errorType };
 }
